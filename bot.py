1080e3c715c7829e1eea3d081a78fb04ba3cbdbc
Bailey Dalton <baileydalton007@gmail.com>
2020-04-14 18:52:01 -0400

    Fixes bug with Date command

--- bot.py
+++ bot.py
@@ -237,7 +237,7 @@ async def date(ctx, name):
     tname = await client.pg_con.fetchrow("SELECT name FROM tournaments WHERE name = $1", name)
     if (tname):
         tdate = await client.pg_con.fetch("SELECT date FROM tournaments WHERE name = $1", name)
-        await ctx.send(f"The closing date of {tname} is {date}")
+        await ctx.send(f"The closing date of {tname} is {tdate}")
     else:
         await ctx.send(f"The tournament {name} does not exist!") 
 
